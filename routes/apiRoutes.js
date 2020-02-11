008d063dd3a568ed5ff9caebe92867a1ff594498
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-02-11 11:25:26 -0700

    getting the server to start up when run with node, /notes still does not work

---
+++ routes/apiRoutes.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const util = require("util");
+var dbData = require("../db/db.json");
+
+
+module.exports = function (app) {
+
+    app.get("/api/db", function (req, res) {
+        res.json(dbData)
+    });
+    app.post("/api/db", function (req, res) {
+
+        console.log(req.body)
+        dbData.push(req.body)
+        res.json(true)
+    });
+}
