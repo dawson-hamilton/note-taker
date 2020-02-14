a6fa49f353192310b42b6e10eacf89998b178914
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-02-13 22:21:20 -0700

    changing app to router

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,11 @@ const path = require("path");
 const router = require("express").Router();
 
 module.exports = function (app) {
-    app.get("*", function (req, res) {
+    router.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    app.get("/notes", function (req, res) {
+    router.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname), "../public/notes.html")
     });
 };
