a92a21bc87c4f33190e7d9a55743e790077a8750
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-02-13 22:08:20 -0700

    finishing touches on the finished product

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,24 @@
-const fs = require("fs");
-const util = require("util");
-var dbData = require("../db/db.json");
+var journal = require("../db/journal.js");
+const router = require("express").Router();
 
 
-module.exports = function (app) {
+router.get("/notes", function (req, res) {
+    journal.getNotes()
+        .then(notes => res.json(notes))
+        .catch(error => res.status(500).json(error))
+});
 
-    app.get("/api/db", function (req, res) {
-        res.json(dbData)
-    });
-    app.post("/api/db", function (req, res) {
+router.post("/notes", function (req, res) {
+    journal.addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(error => res.status(500).json(error))
+});
 
-        console.log(req.body)
-        dbData.push(req.body)
-        res.json(true)
-    });
-}
+router.delete("/notes/:id", function (req, res) {
+    journal.deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(error => res.status(500).json(error))
+});
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-var express = require("express");
+const express = require("express");
+const htmlRoute = require('./routes/htmlRoutes');
+const apiRoute = require('./routes/apiRoutes');
 
-var app = express();
 
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", apiRoute);
+app.use("/", htmlRoute)
 
-require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-});
+    console.log("listening on PORT" + PORT);
+})
