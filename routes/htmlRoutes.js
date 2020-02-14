55f1cadb68d6030d4fccb90428ad1adb4b54f9b6
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-02-13 22:28:46 -0700

    finally figured out the issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,12 @@
-const path = require("path");
+const path = require("path")
 const router = require("express").Router();
 
-module.exports = function (app) {
-    router.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-    router.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"))
-    });
-};
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = router;
